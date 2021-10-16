3ce449b94be1f0ed519b01661f83701ba0a11075
nguye374 <nguye374@sheridancollege.ca>
2021-10-16 03:41:14 -0400

    Oct 16 6th

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
 import './App.css';
-import { useState } from 'react';
+
 import NewTodo from './components/newTodo';
 import Todos from './components/todos';
-import Todo from './models/todo';
+import TodosContextProvider from './store/todos-context';
 
 function App() {
 
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const addTodoHandler = (text: string) => {
-    const newTodo = new Todo(text);
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    })
-  };
-
-  const removeHandler = (id: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter(todo => todo.id !== id);
-    });
-  };
+  
 
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler}/>
-      <Todos items={todos} onRemove={removeHandler}/>
-    </div>
+    <TodosContextProvider>
+      <NewTodo/>
+      <Todos/>
+    </TodosContextProvider>
   );
 }
 
